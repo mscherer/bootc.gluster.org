8730d5e6fe1cbdff0ed3e885fddacdb9c1ba1d81
Michael Scherer <misc@redhat.com>
2026-04-30 17:23:14 +0200

    Adapt the script for AWS

--- set_hostname/set_hostname.sh
+++ set_hostname/set_hostname.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# get hostname from config
-# { echo -e "GET /1.0/meta-data/hostname HTTP/1.0\r\n\r" >&3; cat <&3 ; } 3<> /dev/tcp/169.254.169.254/80  | tail -1
-
-export HOSTNAME=$(curl http://169.254.169.254/1.0/meta-data/hostname)
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/configuring-instance-metadata-service.html
+export TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+export HOSTNAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/1.0/meta-data/hostname)
 
 hostnamectl hostname $HOSTNAME
-
-
